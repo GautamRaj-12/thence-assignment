25eb0648f021227f27082abdf4fa14157256a7fc
Gautam Raj <gautamraj996@gmail.com>
2024-05-07 11:30:23 +0530

    Add congratulations page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Navbar from "./components/Navbar/Navbar";
 import "./assets/fonts/Covered_By_Your_Grace/CoveredByYourGrace-Regular.ttf";
 import "./assets/fonts/Manrope/Manrope-VariableFont_wght.ttf";
+import Congratulations from "./pages/Congratulations/Congratulations";
 // import Hero from "./components/Hero/Hero";
-import Registration from "./pages/Registration";
+// import Registration from "./pages/Registration/Registration";
 const App = () => {
   return (
     <>
       <Navbar />
       {/* <Hero /> */}
-      <Registration />
+      {/* <Registration /> */}
+      <Congratulations />
     </>
   );
 };
---
+++ src/pages/Congratulations/Congratulations.jsx
@@ -0,0 +1,30 @@
+import checkMark from "../../assets/images/check.svg";
+const Congratulations = () => {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center mt-40">
+        <div className="w-[45rem]">
+          <div className="flex justify-center">
+            <img src={checkMark} alt="" />
+          </div>
+          <h1 className="text-[#2DA950] font-[Grace] text-4xl text-center mt-10">
+            success submitted
+          </h1>
+          <h2 className="font-semibold text-[3.5rem] w-[40rem] text-center mx-auto text-[#1C1C1C] mt-4">
+            Congratulations
+          </h2>
+          <p className="text-[#727272] font-medium text-[1.6875rem] mt-5 text-center">
+            Your request has been successfully submitted to us. We will validate
+            your information and reach out to your shortly with updates
+          </p>
+          <p className="mt-[9.75rem] text-center text-[#727272] text-xl">
+            Redirecting you to Homepage in{" "}
+            <span className="text-[#1C1C1C] font-bold">5 seconds</span>
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Congratulations;
