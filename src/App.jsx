bd3e35881ac15e9f3fd6ff5762a75c79563f17f9
Gautam Raj <gautamraj996@gmail.com>
2024-05-07 08:28:37 +0530

    Add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar/Navbar";
-
+import "./assets/fonts/Covered_By_Your_Grace/CoveredByYourGrace-Regular.ttf";
+import "./assets/fonts/Manrope/Manrope-VariableFont_wght.ttf";
 const App = () => {
   return (
     <>
