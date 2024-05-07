91379990861e4d06ac6d16d05caa028eba46dc80
Gautam Raj <gautamraj996@gmail.com>
2024-05-07 16:40:36 +0530

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import Navbar from "./components/Navbar/Navbar";
 import "./assets/fonts/Covered_By_Your_Grace/CoveredByYourGrace-Regular.ttf";
 import "./assets/fonts/Manrope/Manrope-VariableFont_wght.ttf";
+
+import Home from "./pages/Home/Home";
+import Registration from "./pages/Registration/Registration";
 import Congratulations from "./pages/Congratulations/Congratulations";
-// import Hero from "./components/Hero/Hero";
-// import Registration from "./pages/Registration/Registration";
+import Layout from "./Layout";
+
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/register" element={<Registration />}></Route>
+        <Route path="/congratulations" element={<Congratulations />}></Route>
+      </Route>
+    )
+  );
   return (
     <>
-      <Navbar />
-      {/* <Hero /> */}
-      {/* <Registration /> */}
-      <Congratulations />
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 };
