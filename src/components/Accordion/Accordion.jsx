a6080b3352454f6231278a5b9a1235f1034dd457
Gautam Raj <gautamraj996@gmail.com>
2024-05-08 11:12:04 +0530

    Add accordion stying

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,3 +1,7 @@
+import plusIcon from "../../assets/images/plus.svg";
+import minusIcon from "../../assets/images/minus.svg";
+import { useState } from "react";
+
 const Accordion = () => {
   return (
     <>
@@ -27,17 +31,42 @@ const AccordionItems = () => {
       desc: "sample text 4",
     },
   ];
+
+  const [expandedItems, setExpandedItems] = useState(
+    new Array(accordionData.length).fill(false)
+  );
+
+  const handleItemClick = (index) => {
+    const newExpandedItems = [...expandedItems];
+    newExpandedItems[index] = !newExpandedItems[index];
+    setExpandedItems(newExpandedItems);
+  };
+
   return (
     <>
       {accordionData.map((singleItem, index) => (
-        <>
-          <div key={index}>
-            <h1>{singleItem.title}</h1>
-            <p>{singleItem.desc}</p>
+        <div
+          key={index}
+          className="max-w-[33.75rem] mb-16 border-b border-b-[#D7D7D7]"
+        >
+          <div className="pb-6  mt-9 flex justify-between">
+            <h1 className="font-semibold text-xl text-[#1C1C1C]">
+              {singleItem.title}
+            </h1>
+            <img
+              src={expandedItems[index] ? minusIcon : plusIcon}
+              alt="Icon"
+              className="cursor-pointer"
+              onClick={() => handleItemClick(index)}
+            />
           </div>
-        </>
+          {expandedItems[index] && (
+            <p className="text-[#617275] mb-6">{singleItem.desc}</p>
+          )}
+        </div>
       ))}
     </>
   );
 };
+
 export default Accordion;
