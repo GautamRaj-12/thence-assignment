79cb568d4f399daeac44438d0875070bd8723477
Gautam Raj <gautamraj996@gmail.com>
2024-05-08 13:52:43 +0530

    Complete hero section desktop version

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,10 @@
 import heroImage from "../../assets/images/hero-image.png";
 import rightArrow from "../../assets/images/right-arrow.png";
+import rocketIcon from "../../assets/images/rocket.svg";
 const Hero = () => {
   return (
     <section className="ml-16 mt-[5.25rem]">
+      {/* Top */}
       <div>
         <h1 className="text-[#2DA950] font-[Grace] text-4xl text-center">
           success stories
@@ -11,7 +13,10 @@ const Hero = () => {
           Every success journey we&apos;ve encountered.
         </h2>
       </div>
+
+      {/* Bottom */}
       <div className="grid grid-cols-12 mt-28">
+        {/* Left Column */}
         <div className="col-span-7">
           <div className="relative">
             <img
@@ -19,27 +24,46 @@ const Hero = () => {
               alt="A girl looking excited"
               className="mx-auto rounded-[2rem]"
             />
+
+            {/* 40% Part */}
             <div className="w-[16.75rem] h-[15rem] bg-white rounded-[2rem] absolute top-[7rem] left-[9rem]">
-              <p>40%</p>
-              <p>
+              <p className="font-[Switzer] text-[4rem] font-medium mx-auto w-[13rem]">
+                40%
+              </p>
+              <p className="w-[13rem] text-lg font-medium text-[#828282] mx-auto">
                 Achieved reduction in project execution time by optimising team
                 availability
               </p>
             </div>
-            <div className="w-[16.75rem] h-[5.5rem] bg-white rounded-full absolute bottom-[3rem] left-[13rem]">
-              <p>10 DAYS</p>
-              <p>Staff Deployment</p>
+
+            {/* Staff Deployment Part */}
+            <div className="w-[16.75rem] h-[5.5rem] bg-white rounded-full absolute bottom-[3rem] left-[13rem] flex items-center gap-[14px]">
+              <div className="w-[3.25rem] h-[3.25rem] rounded-full bg-[#DDEFE0] flex justify-center items-center ml-4">
+                <img src={rocketIcon} alt="" className="w-8 h-8" />
+              </div>
+              <div>
+                <p className="font-bold text-2xl text-[#1C1C1C]">10 DAYS</p>
+                <p className="text-[#828282] font-medium">Staff Deployment</p>
+              </div>
             </div>
-            <div className="w-[18.125rem] h-[15rem] bg-[#002E18] rounded-[2rem] absolute -bottom-[2.5rem] right-[13rem]">
-              <p>
-                $0.5 <span>Million </span>
-              </p>
-              <p>
+
+            {/* Client Expenses Part */}
+            <div className="w-[18.125rem] h-[15rem] bg-[#002E18] rounded-[2rem] absolute -bottom-[2.5rem] right-[13rem] flex flex-col justify-center items-center gap-[1.25rem]">
+              <div className="flex items-baseline gap-3 mx-auto w-[14rem]">
+                <p className="font-[Switzer] font-bold text-6xl text-white">
+                  $0.5
+                </p>
+                <p className="font-medium text-[22px] text-[#A6A3A0]">
+                  MILLION
+                </p>
+              </div>
+              <p className="text-lg text-[#CCCCCC] mx-auto w-[14rem]">
                 Reduced client expenses by saving on hiring and employee costs.
               </p>
             </div>
           </div>
         </div>
+        {/* Right Column */}
         <div className="col-span-5">
           <p className="text-[2.5rem] font-semibold max-w-[24.875rem] text-[#1C1C1C]">
             Enhance fortune 50 company’s insights teams research capabilities
