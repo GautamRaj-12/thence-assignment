5887e523072c4059010bd3b6215edb71fd695d0f
Gautam Raj <gautamraj996@gmail.com>
2024-05-09 08:21:30 +0530

    Make navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,18 +4,22 @@ const Navbar = () => {
   return (
     <>
       <header>
-        <nav className="m-6 flex flex-wrap justify-between items-center border border-[#EAEAEA] rounded-full">
+        <nav className="m-6 flex md:flex-row flex-col justify-between items-center border border-[#EAEAEA] rounded-full">
           <div>
-            <img src={logo} alt="Brunel Logo" className="my-8 mx-[3.25rem]" />
+            <img
+              src={logo}
+              alt="Brunel Logo"
+              className="my-8 sm:mx-[3.25rem] mx-auto"
+            />
           </div>
-          <div className="flex gap-4 mr-5">
+          <div className="flex gap-4 mr-5 md:mb-0 mb-2 md:ml-0 ml-2">
             <Link
               to="/register"
-              className="text-lg font-medium border border-[#EAEAEA] rounded-full py-7 px-11 hover:bg-[#F1F1F1] transition-all duration-300"
+              className="text-lg font-medium border border-[#EAEAEA] rounded-full sm:py-7 py-3 sm:px-11 px-6 hover:bg-[#F1F1F1] transition-all duration-300"
             >
               Get Projects
             </Link>
-            <button className="text-lg font-semibold bg-[#1C1C1C] text-white rounded-full py-7 px-11 hover:bg-[#4E4E4E] transition-all duration-300">
+            <button className="text-lg font-semibold bg-[#1C1C1C] text-white rounded-full sm:py-7 py-3 sm:px-11 px-6 hover:bg-[#4E4E4E] transition-all duration-300">
               Onboard Talent
             </button>
           </div>
